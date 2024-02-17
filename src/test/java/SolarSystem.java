6091170e0c493a9610ada8fb5c0f43626a85befc
Anton4321G <anton4321garanin@gmail.com>
2024-02-17 11:15:56 -0500

    I have worked on nfeaturesun please rewiew and merge after rewiew

--- src/test/java/SolarSystem.java
+++ src/test/java/SolarSystem.java
@@ -6,5 +6,10 @@ public class SolarSystem {
         solar.stars = "100";
         solar.moon="4";
         solar.planets="8";
+        
+        FeatureSun xros=new FeatureSun();
+        xros.size="2434126";
+        xros.Color="pink";
+        xros.temp="dsfasdfa";
     }
 }
